6371ba5a31ab9182ad732ede45be5c9a9accaeb9
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2026-02-13 14:10:56 +0530

    finalize ui footer and search endpoint

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Link from 'react-router-dom';
 import styles from './Footer.module.css';
 import medifyLogo from '../../assets/logo.jpg'; 
 
-// Import your social icon images
+// Social icons
 import fbIcon from '../../assets/fb.jpg';
 import twitterIcon from '../../assets/twitter.jpg';
 import youtubeIcon from '../../assets/yt.jpg';
@@ -13,24 +14,23 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.topSection}>
-          
           {/* Left Column: Logo & Socials */}
           <div className={styles.brandColumn}>
             <div className={styles.logoWrapper}>
-               <img src={medifyLogo} alt="Medify Logo" className={styles.logoImg} />
+              <img src={medifyLogo} alt="Medify Logo" className={styles.logoImg} />
             </div>
-            
+
             <div className={styles.socialIcons}>
-              <a href="#" className={styles.socialCircle}>
+              <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className={styles.socialCircle}>
                 <img src={fbIcon} alt="Facebook" />
               </a>
-              <a href="#" className={styles.socialCircle}>
+              <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className={styles.socialCircle}>
                 <img src={twitterIcon} alt="Twitter" />
               </a>
-              <a href="#" className={styles.socialCircle}>
-                <img src={youtubeIcon} alt="Youtube" />
+              <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className={styles.socialCircle}>
+                <img src={youtubeIcon} alt="YouTube" />
               </a>
-              <a href="#" className={styles.socialCircle}>
+              <a href="https://www.pinterest.com" target="_blank" rel="noopener noreferrer" className={styles.socialCircle}>
                 <img src={pinterestIcon} alt="Pinterest" />
               </a>
             </div>
@@ -39,32 +39,32 @@ export default function Footer() {
           {/* Right Columns: Links */}
           <div className={styles.linksGrid}>
             <ul className={styles.linkList}>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Our Pricing</a></li>
-              <li><a href="#">Our Gallery</a></li>
-              <li><a href="#">Appointment</a></li>
-              <li><a href="#">Privacy Policy</a></li>
+              <li><Link href="/about-us">About Us</Link></li>
+              <li><Link href="/pricing">Our Pricing</Link></li>
+              <li><Link href="/gallery">Our Gallery</Link></li>
+              <li><Link href="/appointment">Appointment</Link></li>
+              <li><Link href="/privacy-policy">Privacy Policy</Link></li>
             </ul>
             <ul className={styles.linkList}>
-              <li><a href="#">Orthology</a></li>
-              <li><a href="#">Neurology</a></li>
-              <li><a href="#">Dental Care</a></li>
-              <li><a href="#">Ophthalmology</a></li>
-              <li><a href="#">Cardiology</a></li>
+              <li><Link href="/services/orthology">Orthology</Link></li>
+              <li><Link href="/services/neurology">Neurology</Link></li>
+              <li><Link href="/services/dental-care">Dental Care</Link></li>
+              <li><Link href="/services/ophthalmology">Ophthalmology</Link></li>
+              <li><Link href="/services/cardiology">Cardiology</Link></li>
             </ul>
             <ul className={styles.linkList}>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Our Pricing</a></li>
-              <li><a href="#">Our Gallery</a></li>
-              <li><a href="#">Appointment</a></li>
-              <li><a href="#">Privacy Policy</a></li>
+              <li><Link href="/about-us">About Us</Link></li>
+              <li><Link href="/pricing">Our Pricing</Link></li>
+              <li><Link href="/gallery">Our Gallery</Link></li>
+              <li><Link href="/appointment">Appointment</Link></li>
+              <li><Link href="/privacy-policy">Privacy Policy</Link></li>
             </ul>
           </div>
         </div>
 
         {/* Bottom Horizontal Line & Copyright */}
         <div className={styles.bottomSection}>
-          <p>Copyright ©2023 Surya Nursing Home.com. All Rights Reserved</p>
+          <p>Copyright ©2023 Surya Nursing Home. All Rights Reserved</p>
         </div>
       </div>
     </footer>
